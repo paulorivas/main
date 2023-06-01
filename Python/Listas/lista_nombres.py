f39fb0a14eb56e7902baf60549c15b19275ad747
paulo rivas <pau.rivasa@duocuc.cl>
2023-06-01 09:47:02 -0400

    listas

---
+++ Python/Listas/lista_nombres.py
@@ -0,0 +1,11 @@
+lista_nombre = ['Michael', 'Juan']
+lista_nombre.append('Lissette')
+lista_nombre.append('Lissette')
+lista_nombre.append('Lissette')
+lista_nombre.append('Lissette')
+lista_nombre.append('Lissette')
+lista_nombre.append('Lissette')
+print(lista_nombre[0:2])
+
+for x in lista_nombre:
+    print(x)
---
+++ Python/Listas/lista_numeros.py
@@ -0,0 +1,6 @@
+lista_numeros = [[0,1], [2, [3,4]]]
+print(lista_numeros[1][1][1])
+
+lista_numeros2 = [1,2,3,4,5]
+for x in lista_numeros2:
+    print(x)
